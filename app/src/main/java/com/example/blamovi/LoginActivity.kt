a3244116c757100e1639c0caeb7a7e2c9c844ea0
Nicolas Henrique <nicolashenrisilva@outlook.com>
2024-02-04 06:17:54 -0300

    front-end da tela login finalizado!

---
+++ app/src/main/java/com/example/blamovi/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.blamovi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.blamovi.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
