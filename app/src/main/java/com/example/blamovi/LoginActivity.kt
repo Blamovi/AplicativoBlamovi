8416f254a53a824bc902f312749e929f7d2c9c02
Nicolas <nicolashenrisilva@outlook.com>
2024-02-05 21:34:29 -0300

    terminando o top 10

--- app/src/main/java/com/example/blamovi/LoginActivity.kt
+++ app/src/main/java/com/example/blamovi/LoginActivity.kt
@@ -1,12 +1,26 @@
 package com.example.blamovi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.blamovi.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        binding.btnEntrar.setOnClickListener{
+            val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
+
+            startActivity(mainIntent)
+
+            finish()
+        }
+        setContentView(binding.root)
     }
 }
